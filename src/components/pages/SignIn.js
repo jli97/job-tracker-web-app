6dfd890794fd15789e530e6a3c1020cd26ef8399
Jonathan Li <jonathan.li.1997@gmail.com>
2020-07-07 21:18:14 -0400

    added signin options

--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import firebase from '../../config/fbconfig'
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
 class SignIn extends Component {
+    uiConfig = {
+        signInFlow: "redirect",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID
+        ]
+    }
+
     render(){
         return (
             <div>
-                <p>Sign in Page</p>
+                <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
             </div>
         )
     }
